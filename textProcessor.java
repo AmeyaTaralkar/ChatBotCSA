5b44fd1ef06dd5590aab377e4cc68fb7a1519c37
Selynna14 <150946616+Selynna14@users.noreply.github.com>
2025-01-08 08:27:20 -0600

    Update textProcessor.java

--- textProcessor.java
+++ textProcessor.java
@@ -2,31 +2,35 @@ import java.util.*;
 public class textProcessor 
 {
 	private String fileName;
-    private ArrayList<String> textList;
+	private ArrayList<String> textList;
 
     /**
      * Constructor to create a TextProcessor with a file.
      * @param fileName The name of the file to read.
      */
-    public TextProcessor(String fileName) {
-        this.fileName = fileName;
-        this.textList = FileReaderUtil.toStringList(fileName);
-    }
+	public TextProcessor(String filename) 
+	{
+		this.filename = filename;
+		textList = FileReader.toStringList(filename);
+	}
 
     /**
      * Constructor to create a TextProcessor with a list of text.
      * @param textList The list of text.
      */
-    public TextProcessor(ArrayList<String> textList) {
-        this.textList = textList;
-    }
+	   public void TextProcessor(ArrayList<String> textList)
+	   {
+	    	this.textList = textList;
+	   }	
 
     /**
      * Gets the list of text.
      * @return The list of text.
      */
-    public ArrayList<String> getTextList() {
-        return textList;
-    }
+	    public ArrayList<String> getTextList() 
+	    {
+	   	 return textList;
+	    }
+ 
 	
 }
