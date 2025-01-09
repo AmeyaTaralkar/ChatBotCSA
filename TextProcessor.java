128c271e2656126891316fe0982fe061ffb8e504
Selynna14 <150946616+Selynna14@users.noreply.github.com>
2025-01-08 20:18:01 -0600

    Update TextProcessor.java

--- TextProcessor.java
+++ TextProcessor.java
@@ -1,27 +1,35 @@
-import java.util.*;
-public class textProcessor 
-{
-	private String fileName;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * TextProcessor class to handle reading and processing text data.
+ */
+public class TextProcessor 
+{//start class
+    private String fileName;
     private ArrayList<String> textList;
 
     /**
      * Constructor to create a TextProcessor with a file.
      * @param fileName The name of the file to read.
      */
-	public textProcessor(String fileName) 
-	{
-		this.fileName = fileName;
-		textList = FileReader.toStringList(fileName);
-	}
+    public TextProcessor(String fileName) 
+    {
+        this.fileName = fileName;
+        this.textList = new ArrayList<>();
+        readFile();
+    }
 
     /**
      * Constructor to create a TextProcessor with a list of text.
      * @param textList The list of text.
      */
-   public textProcessor(ArrayList<String> textList)
-   {
-    this.textList = textList;
-   }
+    public TextProcessor(ArrayList<String> textList) 
+    {
+        this.textList = textList;
+    }
 
     /**
      * Gets the list of text.
@@ -29,8 +37,25 @@ public class textProcessor
      */
     public ArrayList<String> getTextList() 
     {
-    return textList;
-   }
- 
-	
-}
+        return textList;
+    }
+
+    /**
+     * Reads the file and populates the textList.
+     */
+    private void readFile() 
+    {//start read file
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) 
+        {//start try
+            String line;
+            while ((line = br.readLine()) != null) 
+            {//start while
+                textList.add(line);
+            }//end while
+        }//end try
+        catch (IOException e) 
+        {//start catch
+            System.err.println("Error reading file");
+        }//end catch
+    }//end read file
+}//end class
