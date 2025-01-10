48e3f431808c1759aba8d92ebb866facad37ebd0
AmeyaTaralkar <143042319+AmeyaTaralkar@users.noreply.github.com>
2025-01-10 08:38:27 -0600

    Update ChatBot.java with no errors
    Added another joke and asked the user at the end what they want to talk about. We'll need to use the keyword detection to continue talking with the user.

--- ChatBot.java
+++ ChatBot.java
@@ -1,47 +1,50 @@
 import java.util.*;
 import java.util.regex.Pattern;
-
-public class ChatBot { // start class
-
-      private boolean check;
-      private Scanner scan;
-
-      public ChatBot() {
-         check = true;
-         scan = new Scanner(System.in);
-
-      }
-
-      public void start() {
-         while (check) { // start while loop
-            System.out.println("Hi! Welcome to the companion chatbot! Hahaha. What is your name mate?");
-
-            String nameOfUser = scan.nextLine();
-            System.out.println("Nice to meet you " + nameOfUser + " comrade! How are you today?");
-            String moodInput = scan.nextLine();
-
-            if (moodInput.equalsIgnoreCase("good") || moodInput.equalsIgnoreCase("great") || moodInput.equalsIgnoreCase("well"))
-               System.out.println("That is awesome to hear! Want to hear a joke?");
-            else
-               System.out.println("Can you repeat that? Make sure there are no typos in your response");
-
-            String jokeInput = scan.nextLine();
-            if (jokeInput.equalsIgnoreCase("sure") || jokeInput.equalsIgnoreCase("ok") || jokeInput.equalsIgnoreCase("yes"))
-               System.out.println("Joke: Why shouldn't you fundraise for marathons?");
+public class ChatBot extends TextProcessor { // start class
+   public static void main(String[] args) { // start main method
+      boolean check = true;
+      Scanner scan = new Scanner(System.in);
+      while(check) { // start while loop
+         System.out.println("Hi! Welcome to the funny companion chatbot! Hahaha. What is your name mate?");
+         
+         String nameOfUser = scan.nextLine();
+         if(nameOfUser.equalsIgnoreCase("exit") || nameOfUser.equalsIgnoreCase("goodbye")) {
+            System.out.println("Goodbye user");
+            break;
+         }
+         System.out.println("Nice to meet you " + nameOfUser + " comrade! How are you today?");
+         String moodInput = scan.nextLine();
+         
+         if(moodInput.equalsIgnoreCase("good") || moodInput.equalsIgnoreCase("great") || moodInput.equalsIgnoreCase("well") || moodInput.equalsIgnoreCase("ok"))
+            System.out.println("That is awesome to hear! I am doing well too. Want to hear a joke?");
+         else
+            System.out.println("Can you repeat that? Make sure there are no typos in your response");
+            
+         String jokeInput = scan.nextLine();
+         if(jokeInput.equalsIgnoreCase("sure") || jokeInput.equalsIgnoreCase("ok") || jokeInput.equalsIgnoreCase("yes"))
+            System.out.println("Joke: Why shouldn't you fundraise for marathons?");
             String nextJokeInput = scan.nextLine();
             nextJokeInput = nextJokeInput.toUpperCase();
             char expectedChar = 'W';
             char secondExpectedChar = 'I';
             char firstLetter = nextJokeInput.charAt(0);
-            if (firstLetter == expectedChar || firstLetter == secondExpectedChar)
+            if(firstLetter == expectedChar || firstLetter == secondExpectedChar)
                System.out.println("Because they just take the money and run");
-
-            else if (jokeInput.equalsIgnoreCase("no") || jokeInput.equalsIgnoreCase("nah"))
-               System.out.println("Ok. No joke then. What do you want to learn about?");
-            System.out.println("Do you want to hear another joke? Yes or no?");
+            
+         else {
+            System.out.println("Ok. No joke then. What do you want to learn about?");
+         }
+         System.out.println("Do you want to hear another joke? Yes or no?");
+         String secondJoke = scan.nextLine();
+         boolean jokeQuestion = Pattern.matches("yes", secondJoke.toLowerCase());
+         if(jokeQuestion)
+            System.out.println("How much do rainbows weigh?");
             String secondJokeInput = scan.nextLine();
-            System.out.println(Pattern.matches(secondJokeInput, "yes"));
-
-         } // end while loop
-      }
+            boolean secondJokeQuestion = Pattern.matches(".*\\bhow much\\b.*", secondJokeInput.toLowerCase());
+            if(secondJokeQuestion)
+               System.out.println("Nothing. They're pretty light.");
+            System.out.println("What do you want to talk about " + nameOfUser);
+            String userFirstInput = scan.nextLine();
+      } // end while loop
+   }// end main method
 }// end class
