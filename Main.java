f2bb6a3f9bd0c8c7d34322ed102ca4ef8cdbf45d
AmeyaTaralkar <143042319+AmeyaTaralkar@users.noreply.github.com>
2025-01-13 08:26:46 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+/**
+*The purpose of this program is to create a chatbot that is funny and like a companion
+* and is able to have a conversation with the user
+*
+* @author: Arthur Blasques, Ameya Taralkar, Selynna Song
+* @version: 1.0
+* Date: 01/14/2025
+*/
+
 public class Main { // start class
     public static void main(String[] args) { // start main method
         ChatBot bot = new ChatBot();
