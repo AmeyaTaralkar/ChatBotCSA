371ac2453af5969c6a38eb24e11f879e7b2091b3
AmeyaTaralkar <143042319+AmeyaTaralkar@users.noreply.github.com>
2025-01-12 22:16:00 -0600

    Update TextProcessor.java
    I got rid of detecting from a text file because we won't do that, instead we will be using a keyword detector. We still need to write the logic for the keyword detector but that can be done tomorrow.

--- TextProcessor.java
+++ TextProcessor.java
@@ -5,55 +5,19 @@ import java.io.*;
  * TextProcessor class to handle reading and processing text data.
  */
 public class TextProcessor 
-{//start class
+{ //start class
     private String fileName;
-    private ArrayList<String> textList;
-
-    /**
-     * Constructor to create a TextProcessor with a file.
-     * @param fileName The name of the file to read.
-     */
-    public TextProcessor(String fileName) 
-    {
-        this.fileName = fileName;
-        this.textList = new ArrayList<>();
-        readFile();
-    }
-
-    /**
-     * Constructor to create a TextProcessor with a list of text.
-     * @param textList The list of text.
-     */
-    public TextProcessor(ArrayList<String> textList) 
-    {
-        this.textList = textList;
-    }
-
-    /**
-     * Gets the list of text.
-     * @return The list of text.
-     */
-    public ArrayList<String> getTextList() 
-    {
-        return textList;
-    }
-
+    private ArrayList<String> textList = new ArrayList<>();
     /**
      * Reads the file and populates the textList.
+     * @param userInput The user's chatbot input
      */
-    private void readFile() 
-    {//start read file
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) 
-        {//start try
-            String line;
-            while ((line = br.readLine()) != null) 
-            {//start while
-                textList.add(line);
-            }//end while
-        }//end try
-        catch (IOException e) 
-        {//start catch
-            System.err.println("Error reading file");
-        }//end catch
-    }//end read file
-}//end class
+    public boolean checkKeyWords(String userInput) { //start checkKeyWords method
+      for(String keyword : textList) { //start for loop
+         if(userInput.toLowerCase().contains(keyword)) { // start if statement
+            return true;
+         } // end if statement
+      } // end for loop
+      return false;
+    } //end checkKeyWords method
+} //end class
