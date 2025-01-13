cfa04c12b32c56e13ee1a9ef5643b815caa02b11
AmeyaTaralkar <143042319+AmeyaTaralkar@users.noreply.github.com>
2025-01-12 22:20:51 -0600

    Update ChatBot.java
    Line 51, and 52 are all new lines of code which process the input of the user. However, we have to write the logic for keyword detection.  We are able to store the string input of the user. Next, we need to see if the user input contains a keyword from the keywords.txt. If it does then we have a scripted output for each keyword.

--- ChatBot.java
+++ ChatBot.java
@@ -1,17 +1,17 @@
 import java.util.*;
 import java.util.regex.Pattern;
 public class ChatBot extends TextProcessor { // start class
-   public static void main(String[] args) { // start main method
+   public void start() { // start main method
       boolean check = true;
       Scanner scan = new Scanner(System.in);
       while(check) { // start while loop
          System.out.println("Hi! Welcome to the companion chatbot! Hahaha. What is your name mate?");
          
          String nameOfUser = scan.nextLine();
-         if(nameOfUser.equalsIgnoreCase("exit") || nameOfUser.equalsIgnoreCase("goodbye")) {
+         if(nameOfUser.equalsIgnoreCase("exit") || nameOfUser.equalsIgnoreCase("goodbye")) { // start if statement
             System.out.println("Goodbye user");
             break;
-         }
+         } // end if statement
          System.out.println("Nice to meet you " + nameOfUser + " comrade! How are you today?");
          String moodInput = scan.nextLine();
          
@@ -21,7 +21,7 @@ public class ChatBot extends TextProcessor { // start class
             System.out.println("Can you repeat that? Make sure there are no typos in your response");
             
          String jokeInput = scan.nextLine();
-         if(jokeInput.equalsIgnoreCase("sure") || jokeInput.equalsIgnoreCase("ok") || jokeInput.equalsIgnoreCase("yes"))
+         if(jokeInput.equalsIgnoreCase("sure") || jokeInput.equalsIgnoreCase("ok") || jokeInput.equalsIgnoreCase("yes")) { // start if statement
             System.out.println("Joke: Why shouldn't you fundraise for marathons?");
             String nextJokeInput = scan.nextLine();
             nextJokeInput = nextJokeInput.toUpperCase();
@@ -30,21 +30,26 @@ public class ChatBot extends TextProcessor { // start class
             char firstLetter = nextJokeInput.charAt(0);
             if(firstLetter == expectedChar || firstLetter == secondExpectedChar)
                System.out.println("Because they just take the money and run");
+         } // end if statement
             
-         else {
+         else { // start else
             System.out.println("Ok. No joke then. What do you want to learn about?");
-         }
+         } // end else
          System.out.println("Do you want to hear another joke? Yes or no?");
          String secondJoke = scan.nextLine();
          boolean jokeQuestion = Pattern.matches("yes", secondJoke.toLowerCase());
-         if(jokeQuestion)
+         if(jokeQuestion) { // start outer if statement
             System.out.println("How much do rainbows weigh?");
             String secondJokeInput = scan.nextLine();
             boolean secondJokeQuestion = Pattern.matches(".*\\bhow much\\b.*", secondJokeInput.toLowerCase());
-            if(secondJokeQuestion)
-               System.out.println("Nothing. They're pretty light.");
-            System.out.println("What do you want to talk about? Anything topic you are interested in? " + nameOfUser);
+               if(secondJokeQuestion) { // start inner if statement
+                  System.out.println("Nothing. They're pretty light.");
+               } // end inner if statement
+         } // end outer if statement
+            System.out.println("Ok. What topic do you want to talk about " + nameOfUser + "?");
             String userFirstInput = scan.nextLine();
+            TextProcessor processText = new TextProcessor();
+            processText.checkKeyWords(userFirstInput);
       } // end while loop
    }// end main method
 }// end class
