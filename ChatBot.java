71adc17125b04211e80e309b3b536d9675028c67
CdsmArthur <blasquesarthur8@gmail.com>
2025-01-14 11:16:29 -0600

    Added game method, optimization of code, new keywords, name changes
    Users can now run the game whenever they want to instead of only being able to in the start. After they exit, they're still able to normally chat with the bot as if nothing happened

--- ChatBot.java
+++ ChatBot.java
@@ -29,19 +29,21 @@ public class ChatBot
         nameOfUser = scanner.nextLine();
 		System.out.println("Nice to meet you " + nameOfUser + " comrade! How are you today? Type 'exit' to quit, and 'game' for a surprise.");
         while (true) {
-            System.out.print("You: ");
+            System.out.print(nameOfUser + ": ");
             String userInput = scanner.nextLine();
 
-            if (userInput.equalsIgnoreCase("exit")) {
+            String response = textProcessor.getResponse(userInput);
+             if (userInput.equalsIgnoreCase("exit")) {
                 System.out.println("Goodbye! See you next time!");
                 break;
             }
             
-            else if (userInput.equalsIgnoreCase("game"));
+            else if (userInput.equalsIgnoreCase("game")){
 				game();
-
-            String response = textProcessor.getResponse(userInput);
-            System.out.println("Sertheya: " + response);
+				userInput = "";
+			}
+			else
+				System.out.println("Sertheya: " + response);
         }
 
         scanner.close();
@@ -55,32 +57,91 @@ public class ChatBot
 		
 		int userWin = 0;
 		int botWin = 0;
+		int tie = 0;
 		int round = 0;
-		String input = "";
+		String choice = "";
+		String botChoice;
 		String[] choices = {"rock", "paper", "scissors"};
-		String choice;
-		boolean validInput = false;
+		
+		boolean badInput = true;
+		boolean run = true;
 		
 		System.out.println("Let's play a rock paper scissors game " + nameOfUser + "!");
 		System.out.println("Type 'exit' whenever you want to leave. I'll keep track of scores!");
 		
-		while(input != "exit"){
+		while(run){
+			round++;
 			System.out.println("Make your choice wisely.");
+			System.out.print(nameOfUser + ": ");
 			
-			while(!validInput){
-				input = scanner.nextLine();
-				input.toLowerCase();
-				if (input != "rock" || input!= "paper" || input != "scissors")
+			while(badInput){
+				choice = scanner.nextLine();
+				choice.toLowerCase();
+		
+				if (choice.equalsIgnoreCase("rock") || choice.equalsIgnoreCase("paper") || choice.equalsIgnoreCase("scissors"))
+					badInput = false;
+					
+				else if(choice.equalsIgnoreCase("exit")){
+					round--;
+					System.out.println("Match status: \n" + "Rounds played: " + round + "\nTies: " + tie +"\nBot wins: " + botWin + "\nUser wins: " + userWin + "\n");
+					badInput = false;
+					run = false;
+				}
+					
+				else{
 					System.out.println("Sorry, but your input needs to be 'rock', 'paper', or 'scissors'");
-				else
-					validInput = true;
+					System.out.print(nameOfUser + ": ");
+				}
 			}
 			
+			// Reset variable
+			badInput = true;
 			
+			// Chooses an option for the bot
+			botChoice = choices[((int)(Math.random()*3))];
 			
-			choice = choices[(int)(Math.random()*3)];
+			/*
+			 * Decision making for the winner, giving them
+			 * a point and rerunning the loop again
+			 */
+				
+			if(botChoice.equals(choice)){
+				System.out.println("Tie! No points");
+				tie++;
+			}
+				
+			else if (botChoice.equals("rock")){
+				if (choice.equals("paper")){
+					System.out.println("You win, point for you :(");
+					userWin++;
+				}
+				else if (choice.equals("scissors")){
+					System.out.println("HAHAHA I WIN! Point for me!");
+					botWin++;
+				}
+			}
+			else if (botChoice.equals("paper")){
+				if (choice.equals("scissors")){
+					System.out.println("You win, point for you :(");
+					userWin++;
+				}
+				else if (choice.equals("rock")){
+					System.out.println("HAHAHA I WIN! Point for me!");
+					botWin++;
+				}
+			}
+			else if (botChoice.equals("scissors")){
+				if (choice.equals("rock")){
+					System.out.println("You win, point for you :(");
+					userWin++;
+				}
+				else if (choice.equals("paper")){
+					System.out.println("HAHAHA I WIN! Point for me!");
+					botWin++;
+				}
+			}
+			
 		}
-		
 		
 	}
 }// end class
